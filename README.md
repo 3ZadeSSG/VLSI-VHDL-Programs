610875355aad33e30f83b4107dc716dc1046c889
Manu Gond <manugond45@gmail.com>
2019-08-12 13:10:41 +0530

    fixed directory naming

---
+++ README.md
@@ -0,0 +1,3 @@
+# VLSI-VHDL-Programs
+
+This repo contains the VHDL Programs that I have completed in my M.Sc SEM-III Labs using **Xilinx ISE Design Suite**
---
+++ tempTesting/Shifter.v
@@ -0,0 +1,23 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date:    11:48:30 02/01/2019 
+// Design Name: 
+// Module Name:    Shifter 
+// Project Name: 
+// Target Devices: 
+// Tool versions: 
+// Description: 
+//
+// Dependencies: 
+//
+// Revision: 
+// Revision 0.01 - File Created
+// Additional Comments: 
+//
+//////////////////////////////////////////////////////////////////////////////////
+module Shifter(input [3:0]A,input clk,output [7:0]Result);
+	
+endmodule
